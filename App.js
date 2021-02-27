61ad9e2908ede063245f248ac31ca99233295a94
Asif Mahmud <asif01050105@gmail.com>
2021-02-27 15:28:50 +0600

    package installed: react-navigation, drawer navigator, stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from "expo-status-bar";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import DraweNav from './app/drawer/index'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+        <NavigationContainer> 
+           <DraweNav/>
+        </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
+
