e8b787785390cc85e016877bbc2100af17c8b170
mirsahib <mirsahib24@gmail.com>
2021-03-03 20:52:26 +0600

    :hammer: Add Navigation code

--- app/containers/App/AppContainers.js
+++ app/containers/App/AppContainers.js
@@ -1,7 +1,7 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import DraweNav from "./DrawerNav";
+import DraweNav from "./index";
 export default function AppContainer() {
   return (
     <NavigationContainer>
