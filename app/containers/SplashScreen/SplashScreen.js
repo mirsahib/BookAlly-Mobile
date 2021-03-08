37dce6bce3643f69d64bde0ea947bb29a5c597bf
mirsahib <mirsahib24@gmail.com>
2021-03-08 12:10:09 +0600

    :bug: Fix UI responsiveness

--- app/containers/SplashScreen/SplashScreen.js
+++ app/containers/SplashScreen/SplashScreen.js
@@ -1,44 +1,68 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 import { Button } from "native-base";
 import colors from "../../assets/colors/colors";
 
 export default function SplashScreen({ navigation }) {
   return (
-    <View style={{ backgroundColor: colors.background, flex: 1 }}>
-      <Image
-        source={require("../../assets/images/header_shape.png")}
-        style={styles.header_shape}
-      />
-      <SafeAreaView>
-        <View style={styles.container}>
-          <Image
-            source={require("../../assets/images/undraw_book_reading.png")}
-            style={styles.book_reading}
-          />
-          <Text style={styles.app_moto}>This is our app moto</Text>
-          <Text style={styles.app_para} numberOfLines={5}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur
-            neque orci suspendisse potenti amet, molestie enim. Et arcu volutpat
-            eget ac vestibulum risus ullamcorper. Nunc lacinia justo.
-          </Text>
-          <View style={styles.button_container}>
-            <Button
-              style={styles.button}
-              success
-              full
-              onPress={() => navigation.navigate("Registration")}
-            >
-              <Text style={styles.button_text}> Get Started </Text>
-            </Button>
-          </View>
-        </View>
-      </SafeAreaView>
+    <View style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={require("../../assets/images/header_shape.png")}
+          style={styles.header_shape}
+        />
+      </View>
+      <View
+        style={{
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={require("../../assets/images/undraw_book_reading.png")}
+        />
+      </View>
+      <View
+        style={{
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.app_moto}>This is our app moto</Text>
+        <Text style={styles.app_para} numberOfLines={5}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur
+          neque orci suspendisse potenti amet, molestie enim. Et arcu volutpat
+          eget ac vestibulum risus ullamcorper. Nunc lacinia justo.
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          style={styles.button}
+          success
+          full
+          onPress={() => navigation.navigate("Registration")}
+        >
+          <Text style={styles.button_text}>Get Started</Text>
+        </Button>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  header_shape: {},
+  container: { backgroundColor: colors.background, flex: 1 },
   book_reading: {
     marginLeft: 27,
   },
@@ -47,32 +71,25 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: colors.text_tertiary,
     textAlign: "center",
-    paddingTop: 30,
   },
   app_para: {
     fontFamily: "Poppins-Regular",
     fontSize: 18,
     textAlign: "center",
     color: colors.text_secondary,
-    paddingTop: 20,
+    paddingTop: 10,
   },
   button: {
     backgroundColor: colors.background_secondary,
-    width: 364,
+    width: "95%",
     height: 60,
+    alignSelf: "center",
+    justifyContent: "center",
   },
   button_text: {
     fontFamily: "Poppins-Bold",
     fontSize: 18,
     color: colors.text_white,
-  },
-  button_container: {
-    paddingTop: 50,
-    marginLeft: 15,
-  },
-  text: {
-    color: colors.text_white,
-    fontSize: 18,
-    fontFamily: "Poppins-Bold",
+    textAlign: "center",
   },
 });
